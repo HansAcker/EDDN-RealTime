3d501f987ab2dfc1d27eca45720d96abaa259786
Hans Acker <>
2025-01-09 23:23:16 +0100

    moved sorting into _sort, mangle properties of StatsRow

--- html/js/statsbox.js
+++ html/js/statsbox.js
@@ -4,10 +4,10 @@
 class StatsRow {
 	// TODO: shape-morphism optimization? #value is always a number except for two stats where it's a string
 	#value;
-	key;
+	_key;
 
 	#td;
-	tr;
+	_tr;
 
 	constructor(key, value) {
 		const ktd = document.createElement("td");
@@ -18,17 +18,17 @@ class StatsRow {
 
 		const tr = document.createElement("tr");
 		tr.append(ktd, td);
-		this.tr = tr;
+		this._tr = tr;
 
-		this.key = key;
-		this.value = value;
+		this._key = key;
+		this._value = value;
 	}
 
-	set value(newValue) {
+	set _value(newValue) {
 		this.#td.textContent = this.#value = newValue;
 	}
 
-	get value() {
+	get _value() {
 		return this.#value;
 	}
 }
@@ -54,10 +54,10 @@ class StatsBox {
 
 	set(key, value) {
 		if (this.has(key)) {
-			this._rows[this._stats[key]].value = value;
+			this._rows[this._stats[key]]._value = value;
 		} else {
 			const stat = new StatsRow(key, value);
-			this.#statsbody.append(stat.tr);
+			this.#statsbody.append(stat._tr);
 			this._stats[key] = this._rows.length;
 			this._rows.push(stat);
 		}
@@ -65,7 +65,7 @@ class StatsBox {
 
 	inc(key) {
 		if (this.has(key)) {
-			this._rows[this._stats[key]].value++;
+			this._rows[this._stats[key]]._value++;
 		} else {
 			this.set(key, 1);
 		}
@@ -74,36 +74,44 @@ class StatsBox {
 
 
 // SortedStatsBox keeps _rows and table sorted on inc()
-// TODO: (re-)sort in set()
 
 class SortedStatsBox extends StatsBox {
+	set(key, value) {
+		super.set(key, value);
+		this._sort(key);
+	}
+
 	inc(key) {
-		if (this.has(key)) {
-			const idxOld = this._stats[key];
-			const stat = this._rows[idxOld];
+		super.inc(key);
+		this._sort(key);
+	}
 
-			// update td element
-			stat.value++;
+	// moves a single key upwards
+	_sort(key) {
+		const idxOld = this._stats[key];
 
-			// find new position in rows array
-			const val = stat.value;
-			let idxNew = idxOld;
+		// already at the top
+		if (idxOld === 0) {
+			return;
+		}
 
-			while (idxNew > 0 && val > this._rows[idxNew-1].value) {
-				//console.log(`${key}: ${this._rows[idxNew-1].key} ${this._stats[this._rows[idxNew-1].key]} => ${idxNew}`);
-				this._stats[this._rows[idxNew-1].key] = idxNew--; // update indices
-			}
+		// find new position in rows array
+		const stat = this._rows[idxOld];
+		const val = stat._value;
+		let idxNew = idxOld;
 
-			if (idxNew != idxOld) {
-				//console.log(`${key}: ${this._stats[key]} => ${idxNew}`);
-				this._rows[idxNew].tr.before(stat.tr); // move table row
+		while (idxNew > 0 && val > this._rows[idxNew-1]._value) {
+			//console.log(`${key}: ${this._rows[idxNew-1]._key} ${this._stats[this._rows[idxNew-1]._key]} => ${idxNew}`);
+			this._stats[this._rows[idxNew-1]._key] = idxNew--; // update indices
+		}
 
-				this._rows.copyWithin(idxNew+1, idxNew, idxOld); // shift array back by one
-				this._rows[idxNew] = stat; // re-insert element
-				this._stats[key] = idxNew; // update index
-			}
-		} else {
-			this.set(key, 1);
+		if (idxNew != idxOld) {
+			//console.log(`${key}: ${this._stats[key]} => ${idxNew}`);
+			this._rows[idxNew]._tr.before(stat._tr); // move table row
+
+			this._rows.copyWithin(idxNew+1, idxNew, idxOld); // shift array back by one
+			this._rows[idxNew] = stat; // re-insert element
+			this._stats[key] = idxNew; // update index
 		}
 	}
 }

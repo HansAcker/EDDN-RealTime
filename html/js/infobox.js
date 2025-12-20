e55307717d78667e8675e9c480a0fa594b567a87
Hans Acker <>
2025-12-20 14:55:18 +0100

    use data-action to select infobox action, keep button CSS classes for now

--- html/js/infobox.js
+++ html/js/infobox.js
@@ -1,9 +1,15 @@
 
 // displays message contents
 
+// key: typically a <tr> element
+// val: an EDDN message object
+
+// TODO: use DocumentFragments and importNode()?
+
+
 class InfoBox {
-	#container;
-	#template;
+	#container; // new InfoBox appended here
+	#template; // InfoBox DOM template root node
 	#infoMap = new WeakMap(); // only keep data while the key object exists
 
 	constructor(container, template) {
@@ -11,35 +17,41 @@ class InfoBox {
 		this.#template = template;
 	}
 
-	// TODO: bind or wrap? set() can get called multiple times per message
 	set(key, val) { return this.#infoMap.set(key, val); }
 	get(key) { return this.#infoMap.get(key); }
 	has(key) { return this.#infoMap.has(key); }
 
 	show(key) {
-		if (!this.has(key)) {
+		const msg = this.get(key);
+
+		if (!msg) {
 			return;
 		}
 
-		const msg = this.get(key);
 		const msgText = JSON.stringify(msg, null, 2);
-
 		const infoBox = this.#template.cloneNode(true);
+
 		infoBox.querySelector(".infobox__content").textContent = msgText;
 
 		infoBox.querySelector(".infobox__header").addEventListener("click", (ev) => {
 			ev.stopPropagation();
 
-			if (ev.target.classList.contains("infobox__button--copy-msg")) {
-				navigator.clipboard.writeText(msgText);
-			}
-			else if (ev.target.classList.contains("infobox__button--copy-gts")) {
-				navigator.clipboard.writeText(msg.header.gatewayTimestamp);
-			}
-			else if (ev.target.classList.contains("infobox__button--copy-uid")) {
-				navigator.clipboard.writeText(msg.header.uploaderID);
-			}
-			else { /* if (ev.target.classList.contains("infobox__button--close")) { */
+			// find the action button that was clicked
+			const target = ev.target.closest(".infobox__header [data-infobox__action]");
+			const action = target?.dataset.infobox__action;
+
+			// TODO: use async copy, handle error
+			const actions = {
+				"copy-msg": () => navigator.clipboard.writeText(msgText),
+				"copy-gts": () => navigator.clipboard.writeText(msg.header?.gatewayTimestamp),
+				"copy-uid": () => navigator.clipboard.writeText(msg.header?.uploaderID),
+				"close": () => infoBox.remove(),
+			};
+
+			if (action in actions) {
+				actions[action]();
+			} else {
+				// default action: close on click anywhere in header
 				infoBox.remove();
 			}
 		});

abb2736412204083d5f9cb30b26d31e5cdb489be
Hans Acker <>
2026-01-16 13:17:16 +0100

    WIP: tentatively added new methods

--- html/js/board/Dashboard.js
+++ html/js/board/Dashboard.js
@@ -2,18 +2,19 @@ import { Config } from "#config.js";
 import { InfoBox } from "#ui/infobox.js";
 
 
-import FSDJumpModule from "#modules/FSDJumpModule.js";
-import NavRouteModule from "#modules/NavRouteModule.js";
-import ScanModule from "#modules/ScanModule.js";
-import LocationModule from "#modules/LocationModule.js";
-import CodexEntryModule from "#modules/CodexEntryModule.js";
-import UpdatesModule from "#modules/UpdatesModule.js";
 import ApproachModule from "#modules/ApproachModule.js";
-import EventStatsModule from "#modules/EventStatsModule.js";
+import CodexEntryModule from "#modules/CodexEntryModule.js";
 import EventLogModule from "#modules/EventLogModule.js";
-import VisitsModule from "#modules/VisitsModule.js";
-import NewStarsModule from "#modules/NewStarsModule.js";
+import EventStatsModule from "#modules/EventStatsModule.js";
+import FSDJumpModule from "#modules/FSDJumpModule.js";
+import GameStatsModule from "#modules/GameStatsModule.js";
+import LocationModule from "#modules/LocationModule.js";
+import NavRouteModule from "#modules/NavRouteModule.js";
 import NewBodiesModule from "#modules/NewBodiesModule.js";
+import NewStarsModule from "#modules/NewStarsModule.js";
+import ScanModule from "#modules/ScanModule.js";
+import UpdatesModule from "#modules/UpdatesModule.js";
+import VisitsModule from "#modules/VisitsModule.js";
 
 
 export class Dashboard {
@@ -24,15 +25,12 @@ export class Dashboard {
 	#templates = new Map();
 	#readyPromise = null;
 
-	get ready() { return this.#readyPromise; }
+	#modules = new Map();
 
 
 	constructor(container, router, options) {
 		this.#container = container;
 		this.#router = router;
-
-		console.debug("Dashboard: loading module templates...");
-		this.#readyPromise = this.#loadTemplates();
 
 
 		// use templates from index.html for now
@@ -75,24 +73,63 @@ export class Dashboard {
 			new EventLogModule(router, container.querySelector("#EventLog"), { ...commonOptions, template: templates.querySelector("#EventLogTemplate"), listLength: 30 }),
 
 			// no infobox
-			new EventStatsModule(router, container.querySelector("#EventStats"), { template: templates.querySelector("#EventStatsTemplate") }),
+//			new EventStatsModule(router, container.querySelector("#EventStats"), { template: templates.querySelector("#EventStatsTemplate") }),
+
+			new GameStatsModule(router, container.querySelector("#GameStats tbody")),
 		];
+
+
+		void this.ready;
+	}
+
+
+	get ready() {
+		if (!this.#readyPromise) {
+			this.#readyPromise = this.#loadTemplates();
+		}
+
+		return this.#readyPromise;
 	}
 
 
 	async #loadTemplates() {
-		const response = await fetch(`templates.${encodeURIComponent(Config.templateLocale)}.html`);
+		try {
+			const url =`templates.${encodeURIComponent(Config.templateLocale)}.html`;
 
-		if (!response.ok) {
-			throw new Error("Dashboard: template fetch failed");
+			console.debug(`Dashboard: loading templates from "${url}"...`);
+
+			const response = await fetch(url);
+			if (!response.ok) {
+				throw new Error(`HTTP ${response.status}`);
+			}
+
+			const doc = new DOMParser().parseFromString(await response.text(), "text/html");
+			const templates = doc.querySelectorAll("template[data-dashboard__module]");
+
+			for (const template of templates) {
+				this.#templates.set(template.dataset["dashboard__module"], template);
+			}
+
+			console.debug(`Dashboard: loaded ${templates.length} templates`);
+		}
+		catch (err) {
+				throw new Error("Dashboard: template load failed:", err);
+		}
+	}
+
+
+	#addModule(name, Module, options) {
+		const commonOptions = { infobox: this.#infobox, listLength: Config.listLength ?? 20, ...options };
+
+		const template = this.#templates.get(name);
+		if (!template) {
+			throw new Error(`Dashboard: no template for module name "${name}"`);
 		}
 
-		const doc = new DOMParser().parseFromString(await response.text(), "text/html");
+		const module = new Module({ ...commonOptions, template });
+		const moduleContainer = module._setupContainer();
 
-		for (const template of doc.querySelectorAll("template[data-dashboard__module]")) {
-			this.#templates.set(template.dataset["dashboard__module"], template);
-		}
-
-		console.debug("Dashboard: templates loaded");
+		this.#container.append(moduleContainer);
+		this.#modules.set(name, module);
 	}
 }

7fb7384aa80efc38d7d5647744b98a2dad6dfa0d
Hans Acker <>
2026-02-01 08:41:36 +0100

    _setupContainer() runs in the right class context now while _setupTemplates() still doesn't

--- html/js/board/modules/GameStatsModule.js
+++ html/js/board/modules/GameStatsModule.js
@@ -4,8 +4,7 @@ import { StatsBox } from "#ui/statsbox.js";
 
 
 export class GameStatsModule extends DataTableModule {
-	// TODO: don't declare it here because of the way _setupContainer() is called by super constructor
-	//_statsBox;
+	_statsBox;
 
 	constructor(router, options) {
 		super(router, ["*"], options);
